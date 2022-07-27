94e2e02d19f55a8e9fe8c6229b48e7b0be631e0b
XxISMaxX <jihernandezm@unah.hn>
2022-07-27 01:01:33 -0600

    Primer commit del Proyecto del Grupo 6, con la Conexión de la base de datos creada

---
+++ config/conexion.php
@@ -0,0 +1,18 @@
+<?php
+  class Conectar{
+    protected $dbh;
+
+    protected function Conexion(){
+      try {
+        $conectar = $this->dbh = new PDO("mysql:host=20.216.41.245;dbname=g6_19","G6_19","D7OEynOQ");
+        return $conectar;
+      } catch (Exception $e) {
+        print "¡Error BD!: " . $e->getMessage() . "<br/>";
+        die();
+      }
+    }
+    public function set_names(){
+      return $this->dbh->query("SET NAMES 'utf8'");
+    }
+  }
+?>
